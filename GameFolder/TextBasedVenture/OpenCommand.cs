8c65eda54911b54ac8922080e9be054840e67d41
JacobTaylor14 <95394100+JacobTaylor14@users.noreply.github.com>
2022-04-11 22:03:52 -0400

    Add files via upload

---
+++ GameFolder/TextBasedVenture/OpenCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace DungeonGame
+{
+    public class OpenCommand : Command
+	{
+		public OpenCommand()
+		{
+			this.name = "Open";
+		}
+        override
+        public bool execute(Player player)
+        {
+            if (this.hasSecondWord())
+            {
+                player.open(this.secondWord);
+            }
+            else
+            {
+                player.warningMessage("\nOpen What?");
+            }
+            return false;
+        }
+    }
+}
+
+
